b4f1180373f842f2b18617d56332b7e04f579c38
Stipanovich_Anthony <Stipanovich_Anthony@student.mahoningctc.com>
2020-12-09 12:55:21 -0500

    first commit

---
+++ JS Output Challenge/script.js
@@ -0,0 +1,35 @@
+// Enter your code below to edit index.html
+document.title = "About me (aj)"
+
+yourName = "Aj"
+document.getElementById("yourName").innerHTML = yourName;
+
+myBio = "I am quite the hombre"
+document.getElementById("myBio").innerHTML = myBio;
+
+collegeChoice = "I am not sure what exact college I want to go to but I know I do want to go to one"
+document.getElementById("collegeChoice").innerHTML = collegeChoice;
+
+programChoice = "Computer Science, or IT or something of that nature"
+document.getElementById("programChoice").innerHTML = programChoice;
+
+collegeReason = "I would choose these programs because not only is it something I enjoy doing but the people that are in this field to tend to make some good money"
+document.getElementById("collegeReason").innerHTML = collegeReason;
+
+careerChoice = "The job title I would like to have would be a data scientist or a head software developer or even a game developer, or an IT engineer"
+document.getElementById("careerChoice").innerHTML = careerChoice;
+
+companyChoice = "I would want to work for a large comapany something like google, samsung, apple, amazon, nike, activision or a hospital or something like that"
+document.getElementById("companyChoice").innerHTML = companyChoice;
+
+careerReason = "I would like to work for the big comapnies company's because they would pay better but I would like to work for activision because I have always played call of duty and they are my favorite game company "
+document.getElementById("careerReason").innerHTML = careerReason;
+
+answerOne = " The Document Object Model is a cross-platform and language-independent interface that treats an XML or HTML document as a tree structure wherein each node is an object representing a part of the document. The DOM represents a document with a logical tree."
+document.getElementById("answerOne").innerHTML = answerOne;
+
+answerTwo = "1: Writing into an HTML element, using innerHTML 2:Writing into the HTML output using document.write 3:Writing into an alert box, using window.alert and 4.Writing into the browser console, using console.log  "
+document.getElementById("answerTwo").innerHTML = answerTwo;
+
+answerThree = "All of them  "
+document.getElementById("answerThree").innerHTML = answerThree;
